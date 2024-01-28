5cbf0a98dea5d6e7a5dec0524b2a48f3311c8e38
Anna Chuapetcharasopon <143050387+ahgeak@users.noreply.github.com>
2024-01-27 16:40:11 -0800

    category-routes.js: added comments and cleaned up code

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -1,22 +1,24 @@
 const router = require("express").Router();
 const { Category, Product, Tag } = require("../../models");
 
-// The `/api/categories` endpoint
-
+// find all categories  and return data in JSON
 router.get("/", async (req, res) => {
   // find all categories
   try {
     const categoryData = await Category.findAll({
       // be sure to include its associated Products *** I need to fix this***
-      include: [Product],
+      include: [{ model: Product }]
+
+      // include: [Product],
+
       // I need to figure out how to include product as well as category
-      //   include: [
-      //     {
-      //       model: Product,
-      //       through: Category,
-      //       as: 'product_cateogory'
-      //     }
-      // ]
+        // include: [
+        //   {
+        //     model: Product,
+        //     through: Category,
+        //     as: 'products'
+        //   }
+        // ]
     });
     res.status(200).json(categoryData);
   } catch (err) {
@@ -24,14 +26,14 @@ router.get("/", async (req, res) => {
   }
 });
 
+// find one category using id value entered in the URL and return data in JSON
 router.get("/:id", async (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
   try {
     const categoryData = await Category.findByPk(req.params.id, {
       include: [{ model: Product, as: 'products' }]
     });
 
+    // if there is no categoryData matching the ID, return message
     if (!categoryData) {
       res.status(404).json({ message: 'No category is found with this ID.'});
       return;
@@ -43,8 +45,8 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+// post route to create a new category
 router.post("/", async (req, res) => {
-  // create a new category
   try {
     const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData);
@@ -53,26 +55,22 @@ router.post("/", async (req, res) => {
   }
 });
 
+// put route to update a category by its `id` value
 router.put("/:id", async (req, res) => {
-  // update a category by its `id` value
   try {
-    // const categoryData = await Category.update({
-    //   where: { category_name: req.params.category_name }
-    // })
     const updatedCategory = req.body.category_name;
     const categoryData = await Category.update(
       { category_name: updatedCategory },
       { where: { id: req.params.id }}
       );
-    // categoryData.update({"id": req.body});
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
+// delete route to delete a category by its `id` value
 router.delete("/:id", async (req, res) => {
-  // delete a category by its `id` value
   try {
     const categoryData = await Category.destroy({
       where: { id: req.params.id }
